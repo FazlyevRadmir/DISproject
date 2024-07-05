5e2e28ad01a4541a73e668c546f4c2bbf5d7ca09
Radmir <radmir.fazlyev@gmail.com>
2024-07-05 15:39:08 +0300

    initial commit

---
+++ DISproject/DISproject/SceneDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  SceneDelegate.swift
+//  DISproject
+//
+//  Created by Радмир Фазлыев on 05.07.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: scene.coordinateSpace.bounds)
+        window?.windowScene = scene
+        let rootVC = WelcomeViewController()
+        window?.rootViewController = rootVC
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+
+}
+
---
+++ DISproject/DISproject/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  DISproject
+//
+//  Created by Радмир Фазлыев on 05.07.2024.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
